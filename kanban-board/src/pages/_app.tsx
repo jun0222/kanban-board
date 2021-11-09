af794376e9cc50a144e018ce9a4e1152e9f054ca
JunHatanaka <nextseason6@gmail.com>
2021-11-09 23:28:25 +0900

    Change: Removed default unwanted code.

--- kanban-board/src/pages/_app.tsx
+++ kanban-board/src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app'
-import styles from '../../styles/Home.module.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
